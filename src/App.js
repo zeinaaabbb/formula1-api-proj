c2651110b92baeeb0cb3b46a90b38475e90d2dd3
zeinab <swarsama@hotmail.co.uk>
2024-03-27 17:22:10 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Beyond the Grid 🏎️</h1>
+      <p>Stay Ahead of the Curve with Real-Time Formula 1 Data and Analysis</p>
+    </div>
+  );
+}
+
+export default App;
