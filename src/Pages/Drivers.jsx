63a9def2367d50e76bdbd306a720e9d48f43130f
zeinab <swarsama@hotmail.co.uk>
2024-03-28 20:32:32 +0000

    driver standing fetching

--- src/Pages/Drivers.jsx
+++ src/Pages/Drivers.jsx
@@ -1,7 +1,25 @@
+import { useEffect, useState } from "react"
+
 function Drivers() {
 
+  const [driverStanding, setDriverStanding] = useState([]);
+
+  useEffect(() => {
+    fetch('http://ergast.com/api/f1/current/driverStandings.json')
+    .then(response => response.json())
+    .then((data) => {
+      setDriverStanding(data["MRData"]["StandingsTable"]["StandingsLists"][0]["DriverStandings"]);
+    });
+});
+
   return(
-    <div>Drivers</div>
+    <div className="drivers-container">
+      <div className="drivers-title">
+      <h1>Driver Standing</h1>
+      </div>
+      <div>
+      </div>
+    </div>
   )
 }
 
