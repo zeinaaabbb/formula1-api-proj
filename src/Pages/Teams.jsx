9f79da0bdac66d2f63810e096c85158f4d8caad6
zeinab <swarsama@hotmail.co.uk>
2024-03-29 17:54:12 +0000

    fetching API teams

--- src/Pages/Teams.jsx
+++ src/Pages/Teams.jsx
@@ -1,10 +1,21 @@
+import {useEffect, useState} from "react";
+
+
 function Teams () {
 
+  const [teamStanding, setTeamStanding] = useState([]);
+
+  useEffect(() => {
+    fetch("https://ergast.com/api/f1/current/constructorStandings.json")
+    .then((response) => {return response.json()})
+    .then((data) => console.log(data));
+  }, []);
+
   return (
-    <div>
-      Teams
-    </div>
-    )
+      <div>
+        <h1>Teams Standing</h1>
+      </div>
+      )
 };
 
 export default Teams
