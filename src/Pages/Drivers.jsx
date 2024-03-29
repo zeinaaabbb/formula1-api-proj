e292132cd079d439f336c4303f90648a16b93a57
zeinab <swarsama@hotmail.co.uk>
2024-03-29 16:42:20 +0000

    fixed the cdd using module instead

--- src/Pages/Drivers.jsx
+++ src/Pages/Drivers.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react"
+import styles from "./Drivers.module.css"
 
 function Drivers() {
 
@@ -10,19 +11,49 @@ function Drivers() {
     .then(response => response.json())
     .then((data) => {
       setDriverStanding(data["MRData"]["StandingsTable"]["StandingsLists"][0]["DriverStandings"]);
-      console.log("drivers", driverStanding[0]["Driver"]["familyName"]);
-      console.log("wins", driverStanding[0]["wins"]);
+      // console.log("drivers", driverStanding[0]["Driver"]["familyName"]);
+      // console.log("wins", driverStanding[0]["wins"]);
+      // console.log("drivers", driverStanding);
     });
-}, [driverStanding]);
+}, []);
 
-  return(
-    <div className="drivers-container">
-      <div className="drivers-title">
-      <h1>Driver Standing</h1>
+const currentDate = new Date();
+const year = currentDate.getFullYear();
+
+return(
+    <div className={styles.driverscontainer}>
+      <div className={styles.driverstitle}>
+      <h1>{year} Drivers Standing</h1>
       </div>
 
+      <div>
+      <table className={styles.driverstable}>
+                <tr className={styles.driversheading}>
+                    <th>Position </th>
+                    <th> Name </th>
+                    <th> Points </th>
+                </tr>
+      </table>
+      </div>
+      {driverStanding.map((driver, index) => {
+            const position = driver.position;
+            const familyName = driver.Driver.familyName;
+            const points = driver.points;
+            return (
+            <div>
+              <table>
+                <tr key={index} className={styles.driverscontent}>
+                    <th>{position}</th>
+                    <th>{familyName}</th>
+                    <th>{points}pts</th>
+                </tr>
+              </table>
+            </div>
+            )
+      })
+    }
     </div>
   )
-}
+};
 
 export default Drivers
