65f3864225444a8308f47b867dc5fde8bcec15a4
zeinab <swarsama@hotmail.co.uk>
2024-03-29 17:01:17 +0000

    driver standing looking much better now, phew

--- src/Pages/Drivers.jsx
+++ src/Pages/Drivers.jsx
@@ -20,40 +20,36 @@ function Drivers() {
 const currentDate = new Date();
 const year = currentDate.getFullYear();
 
-return(
-    <div className={styles.driverscontainer}>
-      <div className={styles.driverstitle}>
-      <h1>{year} Drivers Standing</h1>
-      </div>
+return(<div className={styles.driverscontainer}>
+  <div className={styles.driverstitle}>
+  <h1>{year} Drivers Standing</h1>
+  </div>
+  <table className={styles.driverstable}>
+          <thead>
+            <tr className={styles.driversheading}>
+                <th>Position </th>
+                <th> Name </th>
+                <th> Points </th>
+            </tr>
+          </thead>
+          <tbody>
+  {driverStanding.map((driver, index) => {
+        const position = driver.position;
+        const familyName = driver.Driver.familyName;
+        const points = driver.points;
+        return (
+            <tr key={index} className={styles.driverscontent}>
+                <td>{position}</td>
+                <td>{familyName}</td>
+                <td>{points}pts</td>
+            </tr>
 
-      <div>
-      <table className={styles.driverstable}>
-                <tr className={styles.driversheading}>
-                    <th>Position </th>
-                    <th> Name </th>
-                    <th> Points </th>
-                </tr>
-      </table>
-      </div>
-      {driverStanding.map((driver, index) => {
-            const position = driver.position;
-            const familyName = driver.Driver.familyName;
-            const points = driver.points;
-            return (
-            <div>
-              <table>
-                <tr key={index} className={styles.driverscontent}>
-                    <th>{position}</th>
-                    <th>{familyName}</th>
-                    <th>{points}pts</th>
-                </tr>
-              </table>
-            </div>
             )
-      })
-    }
-    </div>
+          })}
+        </tbody>
+        </table>
+        </div>
   )
-};
+  }
 
 export default Drivers
