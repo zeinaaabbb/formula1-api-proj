4692a375442ba4545c12945ace3a23c883813ba3
zeinab <swarsama@hotmail.co.uk>
2024-03-30 16:50:38 +0000

    displaying teams standing

--- src/Pages/Teams.jsx
+++ src/Pages/Teams.jsx
@@ -8,12 +8,40 @@ function Teams () {
   useEffect(() => {
     fetch("https://ergast.com/api/f1/current/constructorStandings.json")
     .then((response) => {return response.json()})
-    .then((data) => console.log(data));
+    .then((data) => setTeamStanding(data.MRData.StandingsTable.StandingsLists[0].ConstructorStandings));
+    console.log("Teams", teamStanding);
   }, []);
 
   return (
       <div>
-        <h1>Teams Standing</h1>
+        <div>
+          <h1>Teams Standing</h1>
+        </div>
+        <div>
+        <table>
+          <thead>
+            <tr>
+              <th>Position</th>
+              <th>Team</th>
+              <th>Points</th>
+            </tr>
+          </thead>
+          <tbody>
+              {teamStanding.map((team, index) => (
+                // console.log("name", team.Constructor.name)
+                // console.log("name", team.position)
+                // console.log("name", team.points)
+                <tr>
+                  <td>{team.position}</td>
+                  <td>{team.Constructor.name}</td>
+                  <td>{team.points}pts</td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
+
+
+        </div>
       </div>
       )
 };
