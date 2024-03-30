d18afc481934f0034449b1da71a71e9772a0bd9b
zeinab <swarsama@hotmail.co.uk>
2024-03-30 23:20:34 +0000

    Race schedule page working

--- src/Pages/Schedule.jsx
+++ src/Pages/Schedule.jsx
@@ -1,8 +1,32 @@
+import { useEffect , useState} from "react"
+
 function Schedule () {
 
+  const [raceSchedule, setRaceSchedule] = useState([]);
+
+  useEffect(() => {
+    fetch("https://ergast.com/api/f1/current.json")
+    .then((response) => {return response.json()})
+    .then((data) => setRaceSchedule(data.MRData.RaceTable.Races));
+  }, [])
+
+
   return (
-    <div>Schedule</div>
-    )
+    <div>
+      <div>
+        <h1>Race Schedule</h1>
+      </div>
+      <div>
+        {raceSchedule.map((race, index) =>(
+          <div>
+            <li key={index}>{race.raceName} - {race.Circuit.circuitName}</li>
+            <li key={index}>{race.date}</li>
+            <li key={index}>{race.round}</li>
+          </div>
+        ))}
+      </div>
+    </div>
+    );
 };
 
 export default Schedule
