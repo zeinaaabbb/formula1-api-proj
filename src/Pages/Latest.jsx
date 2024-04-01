5128afc60155b1ad30813a3dc65bc03dedbcc90f
zeinab <swarsama@hotmail.co.uk>
2024-04-01 12:12:27 +0100

    some styling of text and display

--- src/Pages/Latest.jsx
+++ src/Pages/Latest.jsx
@@ -1,7 +1,31 @@
+import styles from "./Latest.module.css"
+
 function Latest () {
 
   return (
-    <div>Latest</div>
+    <div className={styles.latestcontainer}>
+      <div className={styles.maintitle}>
+        <h1>Latest</h1>
+      </div>
+
+      <div className={styles.lateststitle}>
+        <h1>Last Results</h1>
+      </div>
+
+      <div className={styles.lateststitle}>
+        <h1>Drivers Standing</h1>
+      </div>
+
+      <div className={styles.lateststitle}>
+        <h1>Teams Standing</h1>
+      </div>
+
+      <div className={styles.lateststitle}>
+        <h1>Upcoming Schedule</h1>
+      </div>
+
+    </div>
+
     )
 };
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,8 +7,8 @@ function Navbar () {
     <div className="navbar">
       <h1 className={styles.f1title}>F1: Beyond the Grid 🏎️  </h1>
       <button className={styles.btnnavbar}><Link to="/" className={styles.navlink}>Latest</Link></button>
+      <button className={styles.btnnavbar}><Link to="/results" className={styles.navlink}>Last Results</Link></button>
       <button className={styles.btnnavbar}><Link to="/schedule" className={styles.navlink}>Schedule</Link></button>
-      <button className={styles.btnnavbar}><Link to="/results" className={styles.navlink}>Results</Link></button>
       <button className={styles.btnnavbar}><Link to="/drivers" className={styles.navlink}>Drivers</Link></button>
       <button className={styles.btnnavbar}><Link to="/teams" className={styles.navlink}>Teams</Link></button>
     </div>
